6df43d6b7329ff6422d94a4d352c61f3babb313a
Romano Livi <romano_livi@outlook.com>
2020-12-09 18:06:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,9 +11,10 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip()
   index = input_to_index(input)
-  move(board, index)
-  display_board(board)
-  end
+  if valid_move? == true
+    move(board, index)
+    display_board(board)
+
 end 
 
 def move(board, index, current_player = "X")
