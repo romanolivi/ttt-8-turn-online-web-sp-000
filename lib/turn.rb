07a49d4f1b7f27d7deed2bf9775d66997f60e108
Romano Livi <romano_livi@outlook.com>
2020-12-09 18:08:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,9 +12,10 @@ def turn(board)
   input = gets.strip()
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, current_player)
+    move(board, index)
     display_board(board)
-
+  else 
+    turn(board)
 end 
 
 def move(board, index, current_player = "X")
